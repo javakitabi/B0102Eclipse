bced3e577487604a88ece36dbba1caf4640b2c5b
javakitabi <mustafa@javakitabi.com>
2016-03-08 00:50:43 +0200

    Proje üretildi.

---
+++ src/b0102/Car.java
@@ -0,0 +1,61 @@
+package b0102;
+ 
+/**
+ * Bu örnek, bir araba soyutlamasýnýn (abstraction) nasýl yapýlacaðýný anlatmaktadýr. 
+ * <code>Car</code> sýnýfý, gerçek dünyadaki arabaya karþýlýk gelmekte yani araba soyutlamasýný ifade etmektedir.
+ * <code>Car</code> sýnýfý bir çok deðiskene sahip olup, müþterilerine belli hizmetler sunmaktadýr. 
+ * Bu cümleyi daha doðru bir þekilde, “<code>Car</code> sýnýfýndan üretilen nesnelerin bir durumu ve 
+ * sorumluluklarý vardýr” þeklinde söyleyebiliriz.
+ * 
+ *  
+ * @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
+ * 		   <p>
+ *         Daha fazla bilgi için: 
+ *       	@see  <a href="http://www.javaturk.org" target="_blank">javaturk.org</a>
+ *         
+ * 			@see <a href="http://www.javakitabi.com" target="_blank">javakitabi.com</a>
+ */
+public class Car {
+ 
+    String make;
+    String model;
+    String year;
+    int speed;
+    int distance;
+      
+    /**
+     * Arabanýn kilometresini kendisine geçilen parametre kadar artýrýr. 
+     * 
+     * @param newDistance
+     * 					Arabanýn katettiði yolun uzunluðudur.
+     */
+    public void go(int newDistance){
+        distance += newDistance;
+    }
+     
+    /**
+     * Arabanýn hýzýnda deðiþiklik yapmayý saðlar.
+     * 
+     * @param newSpeed
+     * 					Arabanýn yeni hýzýdýr.	
+     */
+    public void accelerate(int newSpeed){
+        speed = newSpeed;
+    }
+    
+    /**
+     * Arabanýn durmasýný saðlar. 
+     */
+    public void stop(){
+        speed = 0;
+    }
+     
+    /**
+     * Araba hakkýnda bilgi almaya yarar.  
+     * @return 
+     * 			Arabanýn o anki güncel bilgisini verir.
+     */
+    public String getInfo(){
+        return "Car Info: " + year + " " + make + " " + model + ". Distance: " + distance + " km. and traveling at " + speed + " kmph.";
+    }
+}
---
+++ src/b0102/CarTest.java
@@ -0,0 +1,51 @@
+package b0102;
+
+
+/**
+ * Bu sýnýf {@link Car} sýnýfýnýn test sýnýfýdýr. 
+ * Car sýnýfýndan nesneler üretip onlarýn durumlarýný deðiþtirmekte ve onlardan hizmet almaktadýr.
+ * @author <a href="mailto:akin@javaturk.org">Akin Kaldiroglu</a>
+ *         <p>
+ *         Daha fazla bilgi için:
+ * @see <a href="http://www.javaturk.org">javaturk.org</a>
+ * @see <a href="http://www.javakitabi.com">javakitabi.com</a>
+ *      </p>
+ *      
+ */
+public class CarTest {
+ 
+	/**
+	 * Main metodda {@link Car} objeleri üretilir ve durumlarý belirlenir veya deðiþtirilir.
+     *    
+     * @see Car
+     * @param args[] String dizisi.
+     */
+    public static void main(String[] args) {
+ 
+        Car car1 = new Car();
+        System.out.println(car1.getInfo());
+ 
+        car1.make = "Mercedes";
+        car1.model = "C200";
+        car1.year = "2016";
+        car1.distance = 0;
+        car1.speed = 0;
+        String infoCar1 = car1.getInfo();
+		System.out.println(infoCar1);
+        
+        car1.accelerate(125);
+        car1.go(100);
+        car1.go(50);
+        System.out.println(car1.getInfo());
+ 
+        System.out.println();
+ 
+         Car car2 = new Car();
+         car2.make = "BMW";
+         car2.model = "i8";
+         car2.year = "2016";
+         car2.distance = 10000;
+         car2.speed = 160;
+         System.out.println(car2.getInfo());
+    }
+}
